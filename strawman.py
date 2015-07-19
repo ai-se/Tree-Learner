70fcddffcb632edf682ef848f5d15e5b9db47bcc
Rahul Krishna <i.m.ralk@gmail.com>
2015-07-18 20:46:00 -0400

    Stuff...

--- strawman.py
+++ strawman.py
@@ -1,19 +1,45 @@
 from numpy import array, mean, median
+from run import run
+from pdb import set_trace
+from methods1 import createTbl
 
 
-class exemplar():
+class node():
 
   def __init__(self, rows):
     self.rows = array([])
     for r in rows:
-      self.rows.append(array(r.cells))
+      self.rows += array(r.cells)
 
   def centroid(self):
     return median(self.rows)
-  
+
+
 class contrast():
+
   def __init__(self):
-    pass  
+    self.dir = './Jureczko'
+    pass
+
   def delta(self, test):
-    
-    
+    pass
+
+  def nodes(self, rowObject):
+    clusters = set([r.cells[-1] for r in rowObject])
+    for row in rowObject:
+      cluster = []
+      for id in clusters:
+        if row.cells[-1] == id:
+          cluster.append(row)
+      yield node(cluster)
+
+  def main(self):
+    train, test = run(dataName='ant').categorize()
+    train_DF = createTbl(train[-1], isBin=False)._rows
+    test_df = createTbl(test[-1], isBin=False)._rows
+    clusters = [c for c in self.nodes(train_DF)]
+    # -------- DEBUG --------
+    set_trace()
+
+if __name__ == '__main__':
+  contrast().main()
